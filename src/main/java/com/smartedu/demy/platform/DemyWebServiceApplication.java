a47164a484b9c62e2b4ada456cb34955df872af6
Salim Ramirez <salimramirezm@gmail.com>
2025-06-07 15:32:37 -0500

    chore: initial commit

---
+++ src/main/java/com/smartedu/demy/platform/DemyWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartedu.demy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemyWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemyWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartedu/demy/platform/DemyWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartedu.demy.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemyWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
