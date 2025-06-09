9d7a6d6ced7151ef9e98e76d7999b55a354a3f32
Salim Ramirez <salimramirezm@gmail.com>
2025-06-08 20:21:09 -0500

    feat(billing): add InvoiceQueryServiceImpl class in billing context

---
+++ src/main/java/com/smartedu/demy/platform/billing/application/internal/queryservices/InvoiceQueryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.smartedu.demy.platform.billing.application.internal.queryservices;
+
+import com.smartedu.demy.platform.billing.domain.model.aggregates.Invoice;
+import com.smartedu.demy.platform.billing.domain.model.queries.GetInvoiceByStudentIdQuery;
+import com.smartedu.demy.platform.billing.domain.services.InvoiceQueryService;
+import com.smartedu.demy.platform.billing.infrastructure.persistence.jpa.repositories.InvoiceRepository;
+
+import java.util.List;
+
+public class InvoiceQueryServiceImpl implements InvoiceQueryService {
+    private final InvoiceRepository invoiceRepository;
+
+    public InvoiceQueryServiceImpl(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    @Override
+    public List<Invoice> handle(GetInvoiceByStudentIdQuery query) {
+        return invoiceRepository.findByStudentId(query.studentId());
+    }
+}
