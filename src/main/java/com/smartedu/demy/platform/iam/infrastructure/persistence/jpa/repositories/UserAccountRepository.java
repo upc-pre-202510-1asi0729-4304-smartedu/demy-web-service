5d5071958c7b0e7bddfb34b28b71ede71cd0ad9d
Radv2005 <U202312318@upc.edu.pe>
2025-06-16 00:11:55 -0500

    chore(iam): add UserAccount repository for persistence

--- src/main/java/com/smartedu/demy/platform/iam/infrastructure/persistence/jpa/repositories/UserAccountRepository.java
+++ src/main/java/com/smartedu/demy/platform/iam/infrastructure/persistence/jpa/repositories/UserAccountRepository.java
@@ -1,7 +1,18 @@
 package com.smartedu.demy.platform.iam.infrastructure.persistence.jpa.repositories;
 
 import com.smartedu.demy.platform.iam.domain.model.aggregates.UserAccount;
+import com.smartedu.demy.platform.iam.domain.model.resources.Email;
+import com.smartedu.demy.platform.iam.domain.model.valueobjects.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.smartedu.demy.platform.shared.domain.model.valueobjects.UserId;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    List<UserAccount> findByUserId(UserId userId);
+    List<UserAccount> findByRole(Roles roles);
+    Optional<UserAccount> findByEmail_Value(String value);
 }
