2ca1f1ae03e265fbe7846b934ecadb6cb6f9a58d
Radv2005 <U202312318@upc.edu.pe>
2025-06-19 14:49:45 -0500

    fix: Backend iam class diagram

--- src/main/java/com/smartedu/demy/platform/iam/domain/model/commands/SignUpCommand.java
+++
@@ -1,8 +0,0 @@
-package com.smartedu.demy.platform.iam.domain.model.commands;
-
-public record SignUpCommand(
-        String firstName,
-        String lastName,
-        String email,
-        String password
-) {}
