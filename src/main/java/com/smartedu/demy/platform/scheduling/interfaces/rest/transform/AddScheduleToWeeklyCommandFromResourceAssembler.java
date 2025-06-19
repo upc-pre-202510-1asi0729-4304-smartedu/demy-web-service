2c9b3ee9e4ef4a3367ee18d69b4c4504c9c4cf11
DanielCr <u20221g120@upc.edu.pe>
2025-06-19 11:50:00 -0500

    feat(scheduling): add schedule to weekly command from resource assembler

---
+++ src/main/java/com/smartedu/demy/platform/scheduling/interfaces/rest/transform/AddScheduleToWeeklyCommandFromResourceAssembler.java
@@ -0,0 +1,23 @@
+package com.smartedu.demy.platform.scheduling.interfaces.rest.transform;
+
+import com.smartedu.demy.platform.scheduling.domain.model.commands.AddScheduleToWeeklyCommand;
+import com.smartedu.demy.platform.scheduling.interfaces.rest.resources.AddScheduleToWeeklyResource;
+
+public class AddScheduleToWeeklyCommandFromResourceAssembler {
+    /**
+     * Converts an AddScheduleToWeeklyResource to an AddScheduleToWeeklyCommand
+     * @param weeklyScheduleId the weekly schedule ID
+     * @param resource the AddScheduleToWeeklyResource
+     * @return the AddScheduleToWeeklyCommand
+     */
+    public static AddScheduleToWeeklyCommand toCommandFromResource(Long weeklyScheduleId, AddScheduleToWeeklyResource resource) {
+        return new AddScheduleToWeeklyCommand(
+                weeklyScheduleId,
+                resource.startTime(),
+                resource.endTime(),
+                resource.dayOfWeek(),
+                resource.courseId(),
+                resource.classroomId()
+        );
+    }
+}
