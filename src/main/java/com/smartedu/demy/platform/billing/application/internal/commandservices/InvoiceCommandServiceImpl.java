bd744acd1443a7a2bdf4c1f246bd64eb068586d5
Salim Ramirez <salimramirezm@gmail.com>
2025-06-20 00:01:04 -0500

    feat(billing): update InvoiceCommandService

--- src/main/java/com/smartedu/demy/platform/billing/application/internal/commandservices/InvoiceCommandServiceImpl.java
+++ src/main/java/com/smartedu/demy/platform/billing/application/internal/commandservices/InvoiceCommandServiceImpl.java
@@ -1,10 +1,17 @@
 package com.smartedu.demy.platform.billing.application.internal.commandservices;
 
 import com.smartedu.demy.platform.billing.domain.model.aggregates.Invoice;
+import com.smartedu.demy.platform.billing.domain.model.commands.AssignPaymentToInvoiceCommand;
 import com.smartedu.demy.platform.billing.domain.model.commands.CreateInvoiceCommand;
+import com.smartedu.demy.platform.billing.domain.model.entities.Payment;
+import com.smartedu.demy.platform.billing.domain.model.valueobjects.PaymentMethod;
 import com.smartedu.demy.platform.billing.domain.services.InvoiceCommandService;
 import com.smartedu.demy.platform.billing.infrastructure.persistence.jpa.repositories.InvoiceRepository;
+import com.smartedu.demy.platform.shared.domain.model.valueobjects.Money;
 import org.springframework.stereotype.Service;
+
+import java.util.Currency;
+import java.util.Optional;
 
 @Service
 public class InvoiceCommandServiceImpl implements InvoiceCommandService {
@@ -26,5 +33,20 @@ public class InvoiceCommandServiceImpl implements InvoiceCommandService {
             throw new IllegalArgumentException("Error saving invoice", e);
         }
         return invoice.getId();
+    }
+
+    @Override
+    public Payment handle(AssignPaymentToInvoiceCommand command) {
+        var invoice = invoiceRepository.findById(command.invoiceId())
+                .orElseThrow(() -> new IllegalArgumentException("Invoice not found"));
+
+        var money = new Money(command.amount(), Currency.getInstance(command.currency()));
+        var method = PaymentMethod.valueOf(command.method());
+        var payment = new Payment(money, method, command.paidAt());
+
+        invoice.addPayment(payment);
+        invoiceRepository.save(invoice);
+
+        return payment;
     }
 }
--- src/main/java/com/smartedu/demy/platform/billing/domain/services/InvoiceCommandService.java
+++ src/main/java/com/smartedu/demy/platform/billing/domain/services/InvoiceCommandService.java
@@ -1,7 +1,11 @@
 package com.smartedu.demy.platform.billing.domain.services;
 
+import com.smartedu.demy.platform.billing.domain.model.commands.AssignPaymentToInvoiceCommand;
 import com.smartedu.demy.platform.billing.domain.model.commands.CreateInvoiceCommand;
+import com.smartedu.demy.platform.billing.domain.model.entities.Payment;
 
 public interface InvoiceCommandService {
-    Long handle (CreateInvoiceCommand command);
+    Long handle(CreateInvoiceCommand command);
+
+    Payment handle(AssignPaymentToInvoiceCommand command);
 }
