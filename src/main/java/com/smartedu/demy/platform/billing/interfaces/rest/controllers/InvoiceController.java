e9832f94f6c7c40ee1aaab927fc7ed06268ece55
Salim Ramirez <salimramirezm@gmail.com>
2025-06-19 23:22:19 -0500

    feat(billing): add InvoiceController in billing context

--- src/main/java/com/smartedu/demy/platform/billing/interfaces/rest/controllers/InvoiceController.java
+++ src/main/java/com/smartedu/demy/platform/billing/interfaces/rest/controllers/InvoiceController.java
@@ -1,16 +1,18 @@
 package com.smartedu.demy.platform.billing.interfaces.rest.controllers;
 
+import com.smartedu.demy.platform.billing.domain.model.queries.GetInvoiceByIdQuery;
 import com.smartedu.demy.platform.billing.domain.model.queries.GetInvoiceByStudentIdQuery;
+import com.smartedu.demy.platform.billing.domain.services.InvoiceCommandService;
 import com.smartedu.demy.platform.billing.domain.services.InvoiceQueryService;
+import com.smartedu.demy.platform.billing.interfaces.rest.resources.CreateInvoiceResource;
 import com.smartedu.demy.platform.billing.interfaces.rest.resources.InvoiceResource;
+import com.smartedu.demy.platform.billing.interfaces.rest.transform.CreateInvoiceCommandFromResourceAssembler;
 import com.smartedu.demy.platform.billing.interfaces.rest.transform.InvoiceResourceFromEntityAssembler;
 import com.smartedu.demy.platform.shared.domain.model.valueobjects.StudentId;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +23,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "Invoices", description = "Endpoints for invoices")
 public class InvoiceController {
     private final InvoiceQueryService invoiceQueryService;
+    private final InvoiceCommandService invoiceCommandService;
 
-    public InvoiceController(InvoiceQueryService invoiceQueryService) {
+    public InvoiceController(InvoiceQueryService invoiceQueryService, InvoiceCommandService invoiceCommandService) {
         this.invoiceQueryService = invoiceQueryService;
+        this.invoiceCommandService = invoiceCommandService;
     }
 
     @GetMapping("/by-student/{studentId}")
@@ -33,5 +37,17 @@ public class InvoiceController {
                 .map(InvoiceResourceFromEntityAssembler::toResourceFromEntity)
                 .toList();
         return ResponseEntity.ok(invoiceResources);
+    }
+
+    @PostMapping
+    public ResponseEntity<InvoiceResource> createInvoice(@RequestBody CreateInvoiceResource resource) {
+        var createInvoiceCommand = CreateInvoiceCommandFromResourceAssembler.toCommandFromResource(resource);
+        var invoiceId = invoiceCommandService.handle(createInvoiceCommand);
+        var getInvoiceByIdQuery = new GetInvoiceByIdQuery(invoiceId);
+        var invoice = invoiceQueryService.handle(getInvoiceByIdQuery);
+        if (invoice.isEmpty()) return ResponseEntity.notFound().build();
+        var invoiceEntity = invoice.get();
+        var invoiceResource = InvoiceResourceFromEntityAssembler.toResourceFromEntity(invoiceEntity);
+        return new ResponseEntity<>(invoiceResource, HttpStatus.CREATED);
     }
 }
