03b2b76dd3362baf9d6ff220203afb81ce85dd5c
Salim Ramirez <salimramirezm@gmail.com>
2025-07-01 18:44:02 -0500

    feat(billing): add command service method to pay invoices from financial transaction

--- src/main/java/com/smartedu/demy/platform/billing/application/internal/commandservices/FinancialTransactionCommandServiceImpl.java
+++ src/main/java/com/smartedu/demy/platform/billing/application/internal/commandservices/FinancialTransactionCommandServiceImpl.java
@@ -2,15 +2,19 @@ package com.smartedu.demy.platform.billing.application.internal.commandservices;
 
 import com.smartedu.demy.platform.billing.domain.model.aggregates.FinancialTransaction;
 import com.smartedu.demy.platform.billing.domain.model.commands.CreateFinancialTransactionCommand;
+import com.smartedu.demy.platform.billing.domain.model.commands.RegisterPaymentCommand;
 import com.smartedu.demy.platform.billing.domain.model.entities.Payment;
 import com.smartedu.demy.platform.billing.domain.model.valueobjects.PaymentMethod;
 import com.smartedu.demy.platform.billing.domain.model.valueobjects.TransactionCategory;
 import com.smartedu.demy.platform.billing.domain.model.valueobjects.TransactionType;
 import com.smartedu.demy.platform.billing.domain.services.FinancialTransactionCommandService;
 import com.smartedu.demy.platform.billing.infrastructure.persistence.jpa.repositories.FinancialTransactionRepository;
+import com.smartedu.demy.platform.billing.infrastructure.persistence.jpa.repositories.InvoiceRepository;
 import com.smartedu.demy.platform.shared.domain.model.valueobjects.Money;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Currency;
 import java.util.Optional;
 
@@ -18,9 +22,14 @@ import java.util.Optional;
 public class FinancialTransactionCommandServiceImpl implements FinancialTransactionCommandService {
 
     private final FinancialTransactionRepository financialTransactionRepository;
+    private final InvoiceRepository invoiceRepository;
 
-    public FinancialTransactionCommandServiceImpl(FinancialTransactionRepository financialTransactionRepository) {
+    public FinancialTransactionCommandServiceImpl(
+            FinancialTransactionRepository financialTransactionRepository,
+            InvoiceRepository invoiceRepository
+    ) {
         this.financialTransactionRepository = financialTransactionRepository;
+        this.invoiceRepository = invoiceRepository;
     }
 
     @Override
@@ -48,6 +57,46 @@ public class FinancialTransactionCommandServiceImpl implements FinancialTransact
         } catch (Exception e) {
             throw new IllegalArgumentException("Error saving financial transaction", e);
         }
+        return Optional.of(financialTransaction);
+    }
+
+    @Override
+    @Transactional
+    public Optional<FinancialTransaction> handle(RegisterPaymentCommand command) {
+        var invoice = invoiceRepository.findById(command.invoiceId());
+        if (invoice.isEmpty()) {
+            throw new IllegalArgumentException("Invoice not found with invoice id %s".formatted(command.invoiceId()));
+        }
+
+        if (invoice.get().isPaid()) {
+            throw new IllegalStateException("Invoice is already paid");
+        }
+
+        var amount = new Money(invoice.get().getAmount().amount(), Currency.getInstance(invoice.get().getAmount().currency().getCurrencyCode()));
+        var payment = new Payment(
+                command.invoiceId(),
+                amount,
+                PaymentMethod.valueOf(command.method()),
+                LocalDateTime.now()
+        );
+
+        invoice.get().markAsPaid();
+
+        var financialTransaction = new FinancialTransaction(
+                TransactionType.INCOME,
+                TransactionCategory.STUDENTS,
+                "Paid student invoice",
+                LocalDateTime.now(),
+                payment
+        );
+
+        try {
+            invoiceRepository.save(invoice.get());
+            financialTransactionRepository.save(financialTransaction);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error saving invoice or financial transaction", e);
+        }
+
         return Optional.of(financialTransaction);
     }
 }
--- src/main/java/com/smartedu/demy/platform/billing/domain/services/FinancialTransactionCommandService.java
+++ src/main/java/com/smartedu/demy/platform/billing/domain/services/FinancialTransactionCommandService.java
@@ -2,10 +2,13 @@ package com.smartedu.demy.platform.billing.domain.services;
 
 import com.smartedu.demy.platform.billing.domain.model.aggregates.FinancialTransaction;
 import com.smartedu.demy.platform.billing.domain.model.commands.CreateFinancialTransactionCommand;
+import com.smartedu.demy.platform.billing.domain.model.commands.RegisterPaymentCommand;
 
 import java.util.Optional;
 
 public interface FinancialTransactionCommandService {
 
     Optional<FinancialTransaction> handle(CreateFinancialTransactionCommand command);
+
+    Optional<FinancialTransaction> handle(RegisterPaymentCommand command);
 }
