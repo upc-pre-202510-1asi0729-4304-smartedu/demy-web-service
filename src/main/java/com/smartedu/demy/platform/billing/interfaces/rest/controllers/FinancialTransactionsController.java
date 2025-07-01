08fbd9723cda1fd30f827d79b07e0bc24d84b296
Salim Ramirez <salimramirezm@gmail.com>
2025-06-30 22:57:16 -0500

    feat(billing): add FinancialTransactions controller

---
+++ src/main/java/com/smartedu/demy/platform/billing/interfaces/rest/controllers/FinancialTransactionsController.java
@@ -0,0 +1,38 @@
+package com.smartedu.demy.platform.billing.interfaces.rest.controllers;
+
+import com.smartedu.demy.platform.billing.domain.services.FinancialTransactionCommandService;
+import com.smartedu.demy.platform.billing.interfaces.rest.resources.CreateFinancialTransactionResource;
+import com.smartedu.demy.platform.billing.interfaces.rest.resources.FinancialTransactionResource;
+import com.smartedu.demy.platform.billing.interfaces.rest.transform.CreateFinancialTransactionCommandFromResourceAssembler;
+import com.smartedu.demy.platform.billing.interfaces.rest.transform.FinancialTransactionResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/v1/financial-transactions", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Financial Transactions", description = "Endpoints for financial transactions")
+public class FinancialTransactionsController {
+    private final FinancialTransactionCommandService financialTransactionCommandService;
+
+    public FinancialTransactionsController(FinancialTransactionCommandService financialTransactionCommandService) {
+        this.financialTransactionCommandService = financialTransactionCommandService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FinancialTransactionResource> createFinancialTransaction(
+            @RequestBody CreateFinancialTransactionResource resource) {
+        var createFinancialTransactionCommand = CreateFinancialTransactionCommandFromResourceAssembler.toCommandFromResource(resource);
+        var financialTransaction = financialTransactionCommandService.handle(createFinancialTransactionCommand);
+        if (financialTransaction.isEmpty()) return ResponseEntity.badRequest().build();
+        var createdFinancialTransaction = financialTransaction.get();
+        var financialTransactionResource = FinancialTransactionResourceFromEntityAssembler.toResourceFromEntity(createdFinancialTransaction);
+        return new ResponseEntity<>(financialTransactionResource, HttpStatus.CREATED);
+    }
+}
