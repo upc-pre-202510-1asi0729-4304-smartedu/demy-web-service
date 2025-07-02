d1f43f4cc65c45c953599593deecd9041338468f
DanielCr <u20221g120@upc.edu.pe>
2025-07-01 19:40:16 -0500

    feat(scheduling): update Weekly schedule commands and queries

--- src/main/java/com/smartedu/demy/platform/scheduling/domain/model/commands/RemoveScheduleFromWeeklyCommand.java
+++ src/main/java/com/smartedu/demy/platform/scheduling/domain/model/commands/RemoveScheduleFromWeeklyCommand.java
@@ -3,4 +3,14 @@ package com.smartedu.demy.platform.scheduling.domain.model.commands;
 public record RemoveScheduleFromWeeklyCommand(
         Long weeklyScheduleId,
         Long scheduleId
-) {}
+) {
+    public RemoveScheduleFromWeeklyCommand {
+        if (weeklyScheduleId == null || weeklyScheduleId <= 0) {
+            throw new IllegalArgumentException("WeeklyScheduleId cannot be null or less than 1");
+        }
+        if(scheduleId == null || scheduleId <= 0) {
+            throw new IllegalArgumentException("ScheduleId cannot be null or less than 1");
+        }
+
+    }
+}
