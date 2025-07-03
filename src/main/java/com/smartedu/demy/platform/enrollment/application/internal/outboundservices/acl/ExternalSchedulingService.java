b9dbd6fae246fe9b5f26206b3c020e894301fe20
Kyrubi <u20221c486@upc.edu.pe>
2025-07-03 01:51:09 -0500

    refactor(enrollment): rename SchedulingContextFacade to WeeklySchedulesContextFacade

--- src/main/java/com/smartedu/demy/platform/enrollment/application/internal/outboundservices/acl/ExternalSchedulingService.java
+++ src/main/java/com/smartedu/demy/platform/enrollment/application/internal/outboundservices/acl/ExternalSchedulingService.java
@@ -1,21 +1,21 @@
 package com.smartedu.demy.platform.enrollment.application.internal.outboundservices.acl;
 
 import com.smartedu.demy.platform.enrollment.domain.model.valueobjects.WeeklyScheduleId;
+import com.smartedu.demy.platform.scheduling.interfaces.acl.WeeklySchedulesContextFacade;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class ExternalSchedulingService {
-    private final SchedulingContextFacade schedulingContextFacade;
+    private final WeeklySchedulesContextFacade schedulingContextFacade;
 
-    public ExternalSchedulingService(SchedulingContextFacade schedulingContextFacade) {
+    public ExternalSchedulingService(WeeklySchedulesContextFacade schedulingContextFacade) {
         this.schedulingContextFacade = schedulingContextFacade;
     }
 
     public Optional<WeeklyScheduleId> fetchWeeklyScheduleByName(String weeklyScheduleName) {
         var weeklyScheduleId = schedulingContextFacade.fetchWeeklyScheduleIdByName(weeklyScheduleName);
-        return weeklyScheduleId == 0L ? Optional.empty() : Optional.of(weeklyScheduleId);
+        return weeklyScheduleId == 0L ? Optional.empty() : Optional.of(new WeeklyScheduleId(weeklyScheduleId));
     }
 }
