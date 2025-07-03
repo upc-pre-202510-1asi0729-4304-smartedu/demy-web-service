7ec45bc782cf379d7f89b19225b40a1c5e98ccfd
Kyrubi <u20221c486@upc.edu.pe>
2025-07-03 00:29:18 -0500

    refactor(enrollment): adapt EnrollmentResource REST model to expose weeklyScheduleId

--- src/main/java/com/smartedu/demy/platform/enrollment/interfaces/rest/resources/EnrollmentResource.java
+++ src/main/java/com/smartedu/demy/platform/enrollment/interfaces/rest/resources/EnrollmentResource.java
@@ -5,7 +5,7 @@ import com.smartedu.demy.platform.enrollment.domain.model.aggregates.Enrollment;
 import java.math.BigDecimal;
 import java.util.Currency;
 
-public record EnrollmentResource(Long enrollmentId, Long studentId, Long academicPeriodId, BigDecimal amount, Currency currency, String enrollmentStatus, String paymentStatus) {
+public record EnrollmentResource(Long enrollmentId, Long studentId, Long academicPeriodId, Long weeklyScheduleId, BigDecimal amount, Currency currency, String enrollmentStatus, String paymentStatus) {
     public EnrollmentResource {
        if (enrollmentId == null || amount == null || currency == null) {
            throw new IllegalArgumentException("Both enrollmentId and amount and currency are null");
