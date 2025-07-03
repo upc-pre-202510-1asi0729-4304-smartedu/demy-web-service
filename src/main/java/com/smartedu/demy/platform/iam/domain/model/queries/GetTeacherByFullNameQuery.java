b34dd2cbc7cf66e49f794bddc89b34bf2566a4c3
DanielCr <u20221g120@upc.edu.pe>
2025-07-03 12:07:47 -0500

    feat(iam): add query to retrieve teacher by full name

---
+++ src/main/java/com/smartedu/demy/platform/iam/domain/model/queries/GetTeacherByFullNameQuery.java
@@ -0,0 +1,13 @@
+package com.smartedu.demy.platform.iam.domain.model.queries;
+
+
+import com.smartedu.demy.platform.iam.domain.model.valueobjects.FullName;
+
+public record GetTeacherByFullNameQuery (FullName fullname){
+
+}
+
+
+
+
+
