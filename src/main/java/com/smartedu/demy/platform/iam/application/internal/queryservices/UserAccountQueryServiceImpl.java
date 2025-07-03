e053eff3fa5352ec7da956f2b8887707bbfe1d51
DanielCr <u20221g120@upc.edu.pe>
2025-07-03 12:12:03 -0500

    feat(iam): add user account query service by name interface and implement

--- src/main/java/com/smartedu/demy/platform/iam/application/internal/queryservices/UserAccountQueryServiceImpl.java
+++ src/main/java/com/smartedu/demy/platform/iam/application/internal/queryservices/UserAccountQueryServiceImpl.java
@@ -3,6 +3,7 @@ package com.smartedu.demy.platform.iam.application.internal.queryservices;
 import com.smartedu.demy.platform.iam.domain.model.aggregates.UserAccount;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetAllAdminsQuery;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetAllTeachersQuery;
+import com.smartedu.demy.platform.iam.domain.model.queries.GetTeacherByFullNameQuery;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetUserAccountByIdQuery;
 import com.smartedu.demy.platform.iam.domain.model.valueobjects.Roles;
 import com.smartedu.demy.platform.iam.domain.services.UserAccountQueryService;
@@ -34,5 +35,10 @@ public class UserAccountQueryServiceImpl implements UserAccountQueryService {
     @Override
     public Optional<UserAccount> handle(GetUserAccountByIdQuery query) {
         return userAccountRepository.findById(query.id());
+    }
+
+    @Override
+    public Optional<UserAccount> handle(GetTeacherByFullNameQuery query) {
+        return userAccountRepository.findByFullName(query.fullname());
     }
 }
--- src/main/java/com/smartedu/demy/platform/iam/domain/services/UserAccountQueryService.java
+++ src/main/java/com/smartedu/demy/platform/iam/domain/services/UserAccountQueryService.java
@@ -3,6 +3,7 @@ package com.smartedu.demy.platform.iam.domain.services;
 import com.smartedu.demy.platform.iam.domain.model.aggregates.UserAccount;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetAllAdminsQuery;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetAllTeachersQuery;
+import com.smartedu.demy.platform.iam.domain.model.queries.GetTeacherByFullNameQuery;
 import com.smartedu.demy.platform.iam.domain.model.queries.GetUserAccountByIdQuery;
 
 import java.util.List;
@@ -16,4 +17,5 @@ public interface UserAccountQueryService {
 
     Optional<UserAccount> handle(GetUserAccountByIdQuery query);
 
+    Optional<UserAccount> handle(GetTeacherByFullNameQuery query);
 }
