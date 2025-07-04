5f09709fc264d07acd7071ccb1209ac699e9915b
DanielCr <u20221g120@upc.edu.pe>
2025-07-04 11:39:33 -0500

    feat(scheduling): update weekly schedule query service interface and implement

--- src/main/java/com/smartedu/demy/platform/scheduling/application/internal/queryservices/WeeklyScheduleQueryServiceImpl.java
+++ src/main/java/com/smartedu/demy/platform/scheduling/application/internal/queryservices/WeeklyScheduleQueryServiceImpl.java
@@ -2,11 +2,14 @@ package com.smartedu.demy.platform.scheduling.application.internal.queryservices
 
 
 import com.smartedu.demy.platform.scheduling.domain.model.aggregates.WeeklySchedule;
+import com.smartedu.demy.platform.scheduling.domain.model.entities.Schedule;
 import com.smartedu.demy.platform.scheduling.domain.model.queries.GetAllWeeklySchedulesQuery;
+import com.smartedu.demy.platform.scheduling.domain.model.queries.GetSchedulesByTeacherIdQuery;
 import com.smartedu.demy.platform.scheduling.domain.model.queries.GetWeeklyScheduleByIdQuery;
 import com.smartedu.demy.platform.scheduling.domain.model.queries.GetWeeklyScheduleByNameQuery;
 import com.smartedu.demy.platform.scheduling.domain.services.WeeklyScheduleQueryService;
 import com.smartedu.demy.platform.scheduling.infrastructure.persistence.jpa.repositories.WeeklyScheduleRepository;
+import com.smartedu.demy.platform.scheduling.infrastructure.persistence.jpa.repositories.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,9 +19,12 @@ import java.util.Optional;
 public class WeeklyScheduleQueryServiceImpl implements WeeklyScheduleQueryService {
 
     private final WeeklyScheduleRepository weeklyScheduleRepository;
+    private final ScheduleRepository scheduleRepository;
 
-    public WeeklyScheduleQueryServiceImpl(WeeklyScheduleRepository weeklyScheduleRepository) {
+
+    public WeeklyScheduleQueryServiceImpl(WeeklyScheduleRepository weeklyScheduleRepository, ScheduleRepository scheduleRepository) {
         this.weeklyScheduleRepository = weeklyScheduleRepository;
+        this.scheduleRepository= scheduleRepository;
     }
 
     @Override
@@ -30,9 +36,16 @@ public class WeeklyScheduleQueryServiceImpl implements WeeklyScheduleQueryServic
     public Optional<WeeklySchedule> handle(GetWeeklyScheduleByIdQuery query) {
         return weeklyScheduleRepository.findById(query.weeklyScheduleId());
     }
+
     @Override
     public Optional<WeeklySchedule> handle(GetWeeklyScheduleByNameQuery query) {
         return weeklyScheduleRepository.findByName(query.name());
     }
+
+    @Override
+    public List<Schedule> handle(GetSchedulesByTeacherIdQuery query){
+        return scheduleRepository.findByTeacherId(query.teacherId());
+    }
+
 
 }
