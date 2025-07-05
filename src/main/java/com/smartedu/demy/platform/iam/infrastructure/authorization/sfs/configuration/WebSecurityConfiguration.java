ad4d2aea661b8710db1f2f84b32c03782db3449b
Radv2005 <U202312318@upc.edu.pe>
2025-07-05 00:22:28 -0500

    chore(security): update WebSecurityConfiguration to allow payment routes

--- src/main/java/com/smartedu/demy/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/smartedu/demy/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -106,6 +106,7 @@ public class WebSecurityConfiguration {
                         .requestMatchers(
                                 "/api/v1/authentication/**",
                                 "/api/v1/users/sign-in",
+                                "/api/v1/payments/create-intent",
                                 "/api/v1/users/admins/sign-up",
                                 "/api/v1/users/reset-password",
                                 "/v3/api-docs/**",
